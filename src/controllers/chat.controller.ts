abd52fdbdf8d0a09a7f19ee70dc5975372dbc29a
Lithira-Jayanaka <kariyawasamlithira2@gmail.com>
2026-01-04 11:03:08 +0530

    feat: add endpoint to retrieve all active chat participants

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 import { Chat } from "../models/chat.model";
 import { sendSuccess, sendError } from "../utils/api.response.util";
+import { AuthRequest } from "../middlewares/auth.middleware";
 
-export const getChatHistory = async (req: Request, res: Response) => {
+
+export const getChatHistory = async (req:AuthRequest, res: Response) => {
     try {
         const { roomId } = req.params;
         
@@ -13,5 +15,50 @@ export const getChatHistory = async (req: Request, res: Response) => {
         sendSuccess(res, 200, "Chat history retrieved", history);
     } catch (error) {
         sendError(res, 500, "Failed to retrieve chat history");
+    }
+};
+
+export const getAllChatParticipants = async (req: AuthRequest, res: Response) => {
+    try {
+        const chatParticipants = await Chat.aggregate([
+            {
+                $sort: { timeStamp: -1 }
+            },
+            {
+                $group: {
+                    _id: "$roomId",
+                    lastMessage: { $first: "$message" },
+                    lastTimestamp: { $first: "$timeStamp" }
+                }
+            },
+            {
+                $lookup: {
+                    from: "users", 
+                    let: { room_id: "$_id" },
+                    pipeline: [
+                        { $match: { $expr: { $eq: [{ $toString: "$_id" }, "$$room_id"] } } }
+                    ],
+                    as: "userDetails"
+                }
+            },
+            { $unwind: "$userDetails" },
+            {
+                $sort: { lastTimestamp: -1 }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    name: "$userDetails.name",
+                    profileimg: "$userDetails.profileimg",
+                    lastMessage: 1,
+                    lastTimestamp: 1
+                }
+            }
+        ]);
+
+        sendSuccess(res, 200, "Historical chat users retrieved", chatParticipants);
+    } catch (error) {
+        console.error("Aggregation Error:", error);
+        sendError(res, 500, "Failed to retrieve chat participants");
     }
 };
--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getChatHistory } from "../controllers/chat.controller";
+import { getAllChatParticipants, getChatHistory } from "../controllers/chat.controller";
 import { authenticate } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.get("/history/:roomId", authenticate, getChatHistory);
+router.get("/active-users", authenticate, getAllChatParticipants);
 
 export default router;
